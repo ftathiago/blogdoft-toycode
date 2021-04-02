e7a39b28101aac64e719a1f75795a0fe3bef9173
Francisco Thiago de Almeida <ftathiago@gmail.com>
2021-04-02 19:56:16 -0300

    chore: organizing branches

---
+++ ConfigurationReading/src/WebApi/Configurations/DynamicConfigurations.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace WebApi.Configurations
+{
+    public class DynamicConfigurations
+    {
+        public List<Iteration> Configs { get; set; }
+    }
+}
