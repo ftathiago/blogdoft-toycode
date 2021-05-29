7cb8610542dd6c2ae112e6e4de7dc921e0efe634
Francisco Thiago de Almeida <ftathiago@gmail.com>
2021-05-29 17:16:54 -0300

    feat: add solution support

---
+++ WorkingWithTemplates/src/Content/FluentMigrations/MigrationName.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace TemplateNamespace
+{
+    [Migration(18991231235959)]
+    public class MigrationName : Migration
+    {
+        public override void Up()
+        {
+
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
